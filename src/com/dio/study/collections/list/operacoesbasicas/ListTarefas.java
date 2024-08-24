0989090450a64f6bd9987c72e2930239c0c66c68
QuixindoDev <eufrasio.dev@gmail.com>
2024-08-25 00:04:14 +0100

    Initial commit

---
+++ src/com/dio/study/collections/list/operacoesbasicas/ListTarefas.java
@@ -0,0 +1,9 @@
+package com.dio.study.collections.list;
+
+public class ListTarefas {
+    private ListTarefas listTarefas;
+
+    public ListTarefas(ListTarefas listTarefas) {
+        this.listTarefas = listTarefas;
+    }
+}
---
+++ src/com/dio/study/collections/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,13 @@
+package com.dio.study.collections.list.operacoesbasicas;
+
+public class Tarefas {
+    private String descricao;
+
+    public Tarefas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
